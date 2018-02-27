7ba239dece896004410ab2ad9f8ac92392efee3f
Khairy <ahmedkhaled@eseed.net>
2018-02-27 22:31:17 +0200

    Added JSOUP

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String args[]) {
-        System.out.print("Okay");
+        Document doc = Jsoup.parse("https://www.google.com.eg");
+//        Element content = doc.getElementById("content");
+        Elements links = doc.getElementsByTag("a");
+        System.out.print(links.text());
     }
 }
