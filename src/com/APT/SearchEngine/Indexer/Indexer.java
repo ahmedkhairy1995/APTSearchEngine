38ecef2a9af994aee46a67efef3cec192382ab77
Khairy <ahmedkhaled@eseed.net>
2018-02-28 16:14:21 +0200

    Test

--- src/com/APT/SearchEngine/Indexer/Indexer.java
+++ src/com/APT/SearchEngine/Indexer/Indexer.java
@@ -1,13 +1,27 @@
 package com.APT.SearchEngine.Indexer;
 
+import com.APT.SearchEngine.Data.Data;
 import opennlp.tools.stemmer.PorterStemmer;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class Indexer {
     private PorterStemmer porterStemmer=new PorterStemmer();
 
 
     private void index(int index){
-        if(index)
+        if(index>= Data.getDocuments().size())
+            return;
+
+        try{
+            Document document = Jsoup.connect(Data.getDocuments().get(index)).get();
+            Elements elements = document.getAllElements();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
 }
