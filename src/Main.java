c3fd4d3ededb39023fc0ff0f20338ac6fc205ddc
Khairy <ahmedkhaled@eseed.net>
2018-02-28 15:08:06 +0200

    Added MI Libraries for Words

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import opennlp.tools.stemmer.*;
 
 public class Main {
     public static void main(String args[]) {
@@ -9,6 +10,10 @@ public class Main {
             Document document = Jsoup.connect("http://en.wikipedia.org/").get();
             Elements elements = document.getAllElements();
             System.out.print( elements.text()+"\n");
+            PorterStemmer stemmer=new PorterStemmer();
+            System.out.print( stemmer.stem("play")+"\n");
+            System.out.print( stemmer.stem("playing")+"\n");
+            System.out.print( stemmer.stem("joking")+"\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
