4602eef1ff81bf4f69059ab03a4fdd77d0d686b9
Khairy <ahmedkhaled@eseed.net>
2018-02-28 15:32:10 +0200

    Modified Project Structure

---
+++ src/com/APT/SearchEngine/Indexer/Indexer.java
@@ -0,0 +1,9 @@
+package com.APT.SearchEngine.Indexer;
+
+import opennlp.tools.stemmer.PorterStemmer;
+
+public class Indexer {
+    private PorterStemmer porterStemmer=new PorterStemmer();
+
+    
+}
